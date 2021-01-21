5994574a99174eca02e2ea5c6cca21e928326082
chuisiniu <chuisiniu@hotmail.com>
2021-01-21 13:40:00 +0800

    leet 3

---
+++ 00003_length_of_longest_substring.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char *s) {
+    int start = 0;
+    int end = 1;
+    int length = 1;
+    int max_length = length;
+
+    int i;
+    int found;
+
+    if (s[0] == '\0') {
+        return 0;
+    }
+
+    while (s[end] != '\0') {
+        found = 0;
+        for (i = start; i < end; i++) {
+            if (s[end] == s[i]) {
+                start = i + 1;
+                length = end - start + 1;
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found) {
+            length += 1;
+            if (length > max_length) {
+                max_length = length;
+            }
+        }
+
+        end += 1;
+    }
+
+    return max_length;
+}
+
+int lengthOfLongestSubstring1(char *s) {
+    unsigned char *start = s;
+    unsigned char *end = s + 1;
+    int length = 1;
+    int max_length = length;
+    unsigned char bit_map[16];
+    unsigned char *tmp;
+
+    unsigned char arr_pos;
+    unsigned char bit;
+
+    if (*start == '\0') {
+        return 0;
+    }
+
+    bzero(bit_map, sizeof(bit_map));
+    bit_map[*start / 8] |= (1 << (*start % 8));
+
+    while (*end != '\0') {
+        arr_pos = *end >> 3;
+        bit = 1 << (*end & 7);
+        if (bit_map[arr_pos] & bit) {
+            if (length > max_length) {
+                max_length = length;
+            }
+            for (tmp = start; *tmp != *end; tmp++) {
+                bit_map[*tmp >> 3] &= ~(1 << (*tmp & 7));
+            }
+            start = tmp + 1;
+            length = end - start + 1;
+        } else {
+            length += 1;
+            bit_map[arr_pos] |= bit;
+        }
+
+        end += 1;
+    }
+
+    // 整个字符串都没重复
+    if (length > max_length) {
+        return length;
+    }
+
+    return max_length;
+}
+
+int main(int argc, char **argv) {
+    char *str = "pwwkew";
+
+    printf("%d\n", lengthOfLongestSubstring1(str));
+}
