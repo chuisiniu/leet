ef37fcc8c13b419950455fe37c7e2b4ccecc94e3
15222043645 <chuisiniu@hotmail.com>
2021-02-07 18:12:00 +0800

    leet 4 log m+n

--- 00004_find_median_sorted_arrays.c
+++ 00004_find_median_sorted_arrays.c
@@ -66,9 +66,113 @@ double findMedianSortedArrays(int *nums1, int nums1Size,
     return ((double)cur + (double)pre)/ 2;
 }
 
-int main(int argc, char **argv) {
-    int a[] = {2};
-    int b[] = {1, 3, 4};
+double findMedianSortedArrays1(int *nums1, int nums1Size,
+                              int *nums2, int nums2Size) {
+    int curNums1Size = nums1Size;
+    int curNums2Size = nums2Size;
+    int *curStart1 = nums1;
+    int *curStart2 = nums2;
 
-    printf("%f", findMedianSortedArrays(a, 1, b, 3));
+    double result;
+
+    int remain;
+    int kth_min;
+    int half_of_k;
+
+    int i;
+    int j;
+    int *tmp_ptr;
+    int tmp;
+    int candidate1;
+    int candidate2;
+
+    if (nums1Size == 1 && nums2Size == 1) {
+        return (double)(nums1[0] + nums2[0]) / 2;
+    }
+
+#define SET_1_LONGER()                                                  \
+    do {                                                                \
+        if (curNums1Size < curNums2Size) {                              \
+            tmp_ptr = curStart1;                                        \
+            curStart1 = curStart2;                                      \
+            curStart2 = tmp_ptr;                                        \
+            tmp = curNums1Size;                                         \
+            curNums1Size = curNums2Size;                                \
+            curNums2Size = tmp;                                         \
+            tmp = i;                                                    \
+            i = j;                                                      \
+            j = tmp;                                                    \
+        }                                                               \
+    } while(0)
+
+    remain = curNums1Size + curNums2Size;
+    kth_min = remain / 2 + remain % 2;
+
+    do {
+        SET_1_LONGER();
+
+        half_of_k = kth_min / 2;
+        i = (half_of_k < curNums1Size ? half_of_k : curNums1Size) - 1;
+        j = (half_of_k < curNums2Size ? half_of_k : curNums2Size) - 1;
+
+        if (curNums2Size <= 0) {
+            goto ONE_IS_EXHAUSTED;
+        }
+
+        if (curStart1[i] < curStart2[j]) {
+            curStart1 += i + 1;
+            curNums1Size -= i + 1;
+            kth_min -= i + 1;
+            i = 0;
+        } else {
+            curStart2 += j + 1;
+            curNums2Size -= j + 1;
+            kth_min -= j + 1;
+            j = 0;
+        }
+    } while(kth_min > 1);
+
+    SET_1_LONGER();
+
+    if (curNums2Size <= 0) {
+        goto ONE_IS_EXHAUSTED;
+    }
+
+    if (curStart1[i] < curStart2[j]) {
+        result = curStart1[i];
+        if (curNums1Size == 1) {
+            candidate1 = candidate2 = curStart2[j];
+        } else {
+            candidate1 = curStart1[i + 1];
+            candidate2 = curStart2[j];
+        }
+    } else {
+        result = curStart2[j];
+        if (curNums2Size == 1) {
+            candidate1 = candidate2 = curStart1[i];
+        } else {
+            candidate1 = curStart1[i];
+            candidate2 = curStart2[j + 1];
+        }
+    }
+
+    if ((nums1Size + nums2Size) % 2 == 0) {
+        result = (result + (candidate1 < candidate2 ? candidate1 : candidate2)) / 2;
+    }
+
+    return result;
+
+ONE_IS_EXHAUSTED:
+    result = curStart1[kth_min - 1];
+    if ((nums1Size + nums2Size) % 2 == 0) {
+        result = (result + curStart1[kth_min]) / 2;
+    }
+    return result;
+}
+
+int main(int argc, char **argv) {
+    int a[] = {1, 3};
+    int b[] = {2};
+
+    printf("%f", findMedianSortedArrays1(a, 0, b, 1));
 }
