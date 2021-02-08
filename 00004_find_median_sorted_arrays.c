22fc05eccc0b0d26d3b92b0e38e48b99e7242eff
chuisiniu <chuisiniu@hotmail.com>
2021-02-08 10:23:05 +0800

    leet 4 optimize

--- 00004_find_median_sorted_arrays.c
+++ 00004_find_median_sorted_arrays.c
@@ -66,15 +66,13 @@ double findMedianSortedArrays(int *nums1, int nums1Size,
     return ((double)cur + (double)pre)/ 2;
 }
 
+/* findMedianSortedArrays1 使用二分法排除数组前面不可能是中位数的
+ * 数组成员。设两个数组的剩余的个数为remain，中位数是第k小的数kth_min
+ * 那么两个数组中第kth_min / 2个成员较小的前面的成员不可能是中位数
+ */
 double findMedianSortedArrays1(int *nums1, int nums1Size,
-                              int *nums2, int nums2Size) {
-    int curNums1Size = nums1Size;
-    int curNums2Size = nums2Size;
-    int *curStart1 = nums1;
-    int *curStart2 = nums2;
-
-    double result;
-
+                               int *nums2, int nums2Size) {
+    int is_odd;
     int remain;
     int kth_min;
     int half_of_k;
@@ -83,96 +81,98 @@ double findMedianSortedArrays1(int *nums1, int nums1Size,
     int j;
     int *tmp_ptr;
     int tmp;
-    int candidate1;
-    int candidate2;
 
-    if (nums1Size == 1 && nums2Size == 1) {
+    if (1 == nums1Size && 1 == nums2Size) {
         return (double)(nums1[0] + nums2[0]) / 2;
     }
 
-#define SET_1_LONGER()                                                  \
-    do {                                                                \
-        if (curNums1Size < curNums2Size) {                              \
-            tmp_ptr = curStart1;                                        \
-            curStart1 = curStart2;                                      \
-            curStart2 = tmp_ptr;                                        \
-            tmp = curNums1Size;                                         \
-            curNums1Size = curNums2Size;                                \
-            curNums2Size = tmp;                                         \
-            tmp = i;                                                    \
-            i = j;                                                      \
-            j = tmp;                                                    \
-        }                                                               \
+    remain = nums1Size + nums2Size;
+    is_odd = remain % 2;
+    kth_min = remain / 2 + is_odd;
+
+#define SET_1_LONGER()                         \
+    do {                                       \
+        if (nums1Size < nums2Size) {           \
+            tmp_ptr = nums1;                   \
+            nums1 = nums2;                     \
+            nums2 = tmp_ptr;                   \
+            tmp = nums1Size;                   \
+            nums1Size = nums2Size;             \
+            nums2Size = tmp;                   \
+            tmp = i;                           \
+            i = j;                             \
+            j = tmp;                           \
+        }                                      \
     } while(0)
 
-    remain = curNums1Size + curNums2Size;
-    kth_min = remain / 2 + remain % 2;
-
     do {
-        SET_1_LONGER();
-
-        half_of_k = kth_min / 2;
-        i = (half_of_k < curNums1Size ? half_of_k : curNums1Size) - 1;
-        j = (half_of_k < curNums2Size ? half_of_k : curNums2Size) - 1;
-
-        if (curNums2Size <= 0) {
+        if (nums2Size <= 0 || nums1Size <= 0) {
             goto ONE_IS_EXHAUSTED;
         }
 
-        if (curStart1[i] < curStart2[j]) {
-            curStart1 += i + 1;
-            curNums1Size -= i + 1;
-            kth_min -= i + 1;
+        half_of_k = kth_min / 2;
+        i = (half_of_k < nums1Size ? half_of_k : nums1Size) - 1;
+        j = (half_of_k < nums2Size ? half_of_k : nums2Size) - 1;
+
+        // 排除掉nums1[i]和nums2[j]前面的数据
+        if (nums1[i] < nums2[j]) {
+            i++;
+            nums1 += i;
+            nums1Size -= i;
+            kth_min -= i;
             i = 0;
         } else {
-            curStart2 += j + 1;
-            curNums2Size -= j + 1;
-            kth_min -= j + 1;
+            j++;
+            nums2 += j;
+            nums2Size -= j;
+            kth_min -= j;
             j = 0;
         }
     } while(kth_min > 1);
 
-    SET_1_LONGER();
-
-    if (curNums2Size <= 0) {
+    if (nums1Size <= 0 || nums2Size <= 0) {
         goto ONE_IS_EXHAUSTED;
     }
 
-    if (curStart1[i] < curStart2[j]) {
-        result = curStart1[i];
-        if (curNums1Size == 1) {
-            candidate1 = candidate2 = curStart2[j];
+    SET_1_LONGER();
+    if (nums1[i] < nums2[j]) {
+        if (is_odd) {
+            return nums1[i];
+        }
+
+        if (nums1Size == 1) {
+            return (double)(nums1[i] + nums2[j]) / 2;
         } else {
-            candidate1 = curStart1[i + 1];
-            candidate2 = curStart2[j];
+            return (double)(nums1[i] +
+                            (nums1[i + 1] < nums2[j] ? nums1[i + 1]
+                                                     : nums2[j])
+                ) / 2;
         }
     } else {
-        result = curStart2[j];
-        if (curNums2Size == 1) {
-            candidate1 = candidate2 = curStart1[i];
+        if (is_odd) {
+            return nums2[j];
+        }
+
+        if (nums2Size == 1) {
+            return (double)(nums2[j] + nums1[i]) / 2;
         } else {
-            candidate1 = curStart1[i];
-            candidate2 = curStart2[j + 1];
+            return (double)(nums2[j] +
+                            (nums1[i] < nums2[j + 1] ? nums1[i]
+                                                     : nums2[j + 1])
+                            ) / 2;
         }
     }
 
-    if ((nums1Size + nums2Size) % 2 == 0) {
-        result = (result + (candidate1 < candidate2 ? candidate1 : candidate2)) / 2;
-    }
-
-    return result;
-
 ONE_IS_EXHAUSTED:
-    result = curStart1[kth_min - 1];
-    if ((nums1Size + nums2Size) % 2 == 0) {
-        result = (result + curStart1[kth_min]) / 2;
-    }
-    return result;
+    SET_1_LONGER();
+
+    return is_odd ? nums1[kth_min - 1]
+                  : (double)(nums1[kth_min - 1] + nums1[kth_min]) / 2;
 }
 
 int main(int argc, char **argv) {
     int a[] = {1, 3};
-    int b[] = {2};
+    int b[] = {2, 3};
 
-    printf("%f", findMedianSortedArrays1(a, 0, b, 1));
+    printf("%f", findMedianSortedArrays1(a, 1, b, 2));
 }
