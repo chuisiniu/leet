f81be4af6259bd8c34fea9b1e2296c5e28bbed2b
chuisiniu <chuisiniu@hotmail.com>
2021-02-08 16:27:00 +0800

    leet 5 动态优化

--- 00005_longest_palindrome.c
+++ 00005_longest_palindrome.c
@@ -17,6 +17,8 @@
  *      1 <= s.length <= 1000
  *      s 仅由数字和英文字母（大写和/或小写）组成
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 char *longestPalindrome(char *s) {
     int i;
@@ -63,4 +65,61 @@ char *longestPalindrome(char *s) {
     s[max_end + 1] = '\0';
 
     return s + max_start;
+}
+
+char *longestPalindrome1(char *s) {
+    unsigned char state[1024][1024] = {0};
+    int i;
+    int j;
+    int len;
+    int max_i;
+    int max_j;
+    int found;
+    int not_found_count;
+
+    max_i = 0;
+    max_j = 0;
+    found = 0;
+    for (len = 1; s[len - 1] != '\0'; len++) {
+        found = 0;
+        for (i = 0, j = i + len - 1; s[j] != '\0'; i++, j++) {
+            if (s[i] != s[j]) {
+                state[i][j] = 0;
+            } else if (j - i < 3) {
+                state[i][j] = 1;
+                if (0 == found) {
+                    max_i = i;
+                    max_j = j;
+                    found = 1;
+                    not_found_count = 0;
+                }
+            } else {
+                state[i][j] = state[i + 1][j - 1];
+                if (1 == state[i][j] && 0 == found) {
+                    max_i = i;
+                    max_j = j;
+                    found = 1;
+                    not_found_count = 0;
+                }
+            }
+        }
+        if (not_found_count == 2) {
+            break;
+        }
+        if (0 == found) {
+            not_found_count++;
+        }
+    }
+
+    s[max_j + 1] = 0;
+
+    return s + max_i;
+}
+
+int main(int argc, char **argv) {
+  char s[] = "bacabab";
+
+  printf("%s\n", longestPalindrome1(s));
+
+  exit(0);
 }
