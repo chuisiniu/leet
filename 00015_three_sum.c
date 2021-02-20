5d45aa197ca5c8fb665e75c9e2bc7f4df45885a7
chuisiniu <chuisiniu@hotmail.com>
2021-02-20 15:08:31 +0800

    leet 15优化

--- 00015_three_sum.c
+++ 00015_three_sum.c
@@ -86,24 +86,6 @@ int bifind(int *nums, int numsSize, int target) {
     return mid + 1 + tmp;
 }
 
-void find_start_end(int *nums, int numsSize, int *neg_search_end, int *pos_search_start) {
-    *neg_search_end = 0;
-    while (*neg_search_end < numsSize && nums[*neg_search_end] < 0) {
-        (*neg_search_end)++;
-    }
-    if (*neg_search_end < numsSize && *neg_search_end > 0 && nums[*neg_search_end] != 0) {
-        (*neg_search_end)--;
-    }
-
-    *pos_search_start = numsSize - 1;
-    while (*pos_search_start > 0 && nums[*pos_search_start] > 0) {
-        (*pos_search_start)--;
-    }
-    if (*pos_search_start >=0 && *pos_search_start < numsSize - 1 && nums[*pos_search_start] != 0) {
-        (*pos_search_start)++;
-    }
-}
-
 #define MAX_MALLOC_SIZE 81920
 
 /* 要保证a <= b <= c */
@@ -132,8 +114,6 @@ int **threeSum(int *nums, int numsSize, int *returnSize,
     int j;
     int m;
     int tmp;
-    int neg_search_end;
-    int pos_search_start;
 
     int start;
     int end;
@@ -148,7 +128,6 @@ int **threeSum(int *nums, int numsSize, int *returnSize,
     }
 
     quick_sort(nums, numsSize);
-    find_start_end(nums, numsSize, &neg_search_end, &pos_search_start);
 
     malloc_size = (long long)numsSize * (numsSize - 1) * (numsSize - 2) / 6;
     malloc_size = malloc_size < MAX_MALLOC_SIZE ? malloc_size : MAX_MALLOC_SIZE;
@@ -164,7 +143,7 @@ int **threeSum(int *nums, int numsSize, int *returnSize,
     while (i < numsSize && j >= 0 && nums[i] < 0 && nums[j] > 0) {
         if (-nums[i] >= nums[j]) {
             // - + +   - 0 +
-            start = pos_search_start;
+            start = i;
             for (m = j; nums[m] > 0;) {
                 tmp = 0 - nums[i] - nums[m];
                 if (tmp <= nums[m] && -1 != (tmp = bifind(nums + start, m - start, tmp))) {
@@ -185,7 +164,7 @@ int **threeSum(int *nums, int numsSize, int *returnSize,
             }
         } else {
             // - - +
-            end = neg_search_end;
+            end = j;
             for (m = i; nums[m] < 0;) {
                 tmp = 0 - nums[j] - nums[m];
                 if (tmp >= nums[m] && -1 != (tmp = bifind(nums + m + 1, end - m, tmp))) {
@@ -210,6 +189,63 @@ int **threeSum(int *nums, int numsSize, int *returnSize,
         && (nums[i] == 0 && nums[i + 1] == 0 && nums[i + 2] == 0
             || nums[j] == 0 && nums[j - 1] == 0 && nums[j - 2] == 0)) {
         add_result(result, *returnColumnSizes, returnSize, 0, 0, 0);
+    }
+
+    return result;
+}
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume
+ * caller calls free().
+ */
+int **threeSum1(int *nums, int numsSize, int *returnSize,
+               int **returnColumnSizes) {
+    int i;
+    int sum;
+    int tmp;
+
+    int start;
+    int end;
+
+    int **result;
+    long long malloc_size;
+
+    *returnSize = 0;
+    *returnColumnSizes = 0;
+    if (numsSize < 3) {
+        return 0;
+    }
+
+    quick_sort(nums, numsSize);
+
+    malloc_size = (long long)numsSize * (numsSize - 1) * (numsSize - 2) / 6;
+    malloc_size = malloc_size < MAX_MALLOC_SIZE ? malloc_size : MAX_MALLOC_SIZE;
+    result = malloc(malloc_size * sizeof(int *));
+    *returnColumnSizes = malloc(malloc_size * sizeof(int));
+    for (i = 0; i < malloc_size; i++) {
+        result[i] = 0;
+        (*returnColumnSizes)[i] = 0;
+    }
+
+    for (i = 0; i < numsSize && nums[i] <= 0;) {
+        start = i + 1;
+        end = numsSize - 1;
+        while (start < end) {
+            sum = nums[i] + nums[start] + nums[end];
+            if (sum == 0) {
+                add_result(result, *returnColumnSizes, returnSize,
+                           nums[i], nums[start], nums[end]);
+                for (tmp = nums[start]; start < end && tmp == nums[++start];) {}
+                for (tmp = nums[end]; start < end && tmp == nums[--end];) {}
+            } else if (sum < 0) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        for (tmp = nums[i]; i < numsSize && tmp == nums[i];) {i++;}
     }
 
     return result;
